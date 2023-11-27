cdfc9843434abe20af699488865110c4d077c93c
Rafael <rveigacar@gmail.com>
2023-11-27 23:13:14 +0100

    Updates

---
+++ precache-manifest.782bc892c8fa8699ed2aea16fad5fd15.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70fec7581dfe4d57d1e5e1f694ad3a9c",
+    "url": "/fcc-random-quote-generator/index.html"
+  },
+  {
+    "revision": "5ac88c92dddde4f5a0d2",
+    "url": "/fcc-random-quote-generator/static/css/main.ca286c92.chunk.css"
+  },
+  {
+    "revision": "e9c0a7a92bee81f9b7c0",
+    "url": "/fcc-random-quote-generator/static/js/2.07cbe438.chunk.js"
+  },
+  {
+    "revision": "1d46a1aa274981c32a89b02d7c469cbc",
+    "url": "/fcc-random-quote-generator/static/js/2.07cbe438.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d2a08e4bb7e22a2427e",
+    "url": "/fcc-random-quote-generator/static/js/3.2d931612.chunk.js"
+  },
+  {
+    "revision": "5ac88c92dddde4f5a0d2",
+    "url": "/fcc-random-quote-generator/static/js/main.3a5e49f0.chunk.js"
+  },
+  {
+    "revision": "197955957c911c8e8338",
+    "url": "/fcc-random-quote-generator/static/js/runtime-main.6e7efbd2.js"
+  }
+]);
